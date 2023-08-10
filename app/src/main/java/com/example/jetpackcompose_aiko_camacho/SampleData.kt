c9268eaf6042b7ef8aed259422e50f2405f2d480
efWGwegwg <Aprendiz@BOGDFPCGMF4090.FormacionSena.RED>
2023-08-10 17:16:51 -0500

    JetpackCompose_Aiko

---
+++ app/src/main/java/com/example/jetpackcompose_aiko_camacho/SampleData.kt
@@ -0,0 +1,73 @@
+package com.example.jetpackcompose_aiko_camacho
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        MainActivity.Message(
+            "Machape",
+            "Record man play my song on the radio..."
+        ),
+        MainActivity.Message(
+            "Machape",
+            "List of The WEEKND ALBUMS:\n" +
+                    "StarBoy (2016)\n" +
+                    "After Hours (2020)\n" +
+                    "The Idol  (2023)\n" +
+                    "Dawn FM (2022)\n" +
+                    "House of balloons (2011)\n" +
+                    "Triology (2012)\n" +
+                    "Kiss Land (2013)\n" +
+                    "Thursday (2011)\n" +
+                    "Beauty Behind The Madness (2015)\n"
+        ),
+        MainActivity.Message(
+            "Machape",
+            " Sing whit me : They say only the good die young.\n" +
+                    "That just ain't right!"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "'Cause we're having too much fun..."
+        ),
+        MainActivity.Message(
+            "Machape",
+            "Too much fun tonight, yeah!\n" +
+                    "And a lust for life, and a lust for life." +
+                    "And a lust for life, and a lust for life." +
+                    "Keeps us alive, keeps us alive <3"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "Then, we dance on the H of the Hollywood sign, yeah"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "'Til we run out of breath, gotta dance 'til we die"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "My boyfriend's back"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "And he's cooler than eveeerr"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "There's no more night, blue skies forever"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "'Cause we're the masters of our own fate"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "We're the captains of our own souls"
+        ),
+        MainActivity.Message(
+            "Machape",
+            "So there's no need for us to hesitate,We're all alone, let's take control\n" +
+                    "And I was like"
+        ),
+    )
+}
